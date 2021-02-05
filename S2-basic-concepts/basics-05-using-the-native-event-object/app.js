b0058014b69efef2c6891aa1f456e90e911e3888
Matija Osrecki <matija.osrecki@gmail.com>
2021-02-05 15:29:01 +0100

    learned about computed

--- S2-basic-concepts/basics-05-using-the-native-event-object/app.js
+++ S2-basic-concepts/basics-05-using-the-native-event-object/app.js
@@ -3,20 +3,64 @@ const app = Vue.createApp({
 		return {
 			counter: 0,
 			name: '',
+			confirmedName: '',
 		};
 	},
 	methods: {
-		setName(event, lastName) {
-			this.name = event.target.value + ' ' + lastName;
+		calculateTime() {
+			const now = new Date();
+			const hours = now.getHours();
+			const minutes = now.getMinutes();
+			const seconds =
+				now.getSeconds() < 10
+					? '0' + now.getSeconds()
+					: now.getSeconds();
+			const time = `${hours}:${minutes}:${seconds}`;
+			return time;
+		},
+		setName(event) {
+			this.name = event.target.value;
+		},
+		confirmName() {
+			this.confirmedName = this.name;
 		},
 		add(num) {
 			this.counter = this.counter + num;
 		},
 		reduce(num) {
 			this.counter = this.counter - num;
-			// this.counter--;
+		},
+		resetInput() {
+			this.name = '';
+		},
+		methodDate() {
+			return 'fafdsfds';
+		},
+	},
+	// computed is like a data property (YOU DONT CALL IT), name it like property, not method
+	computed: {
+		fullName() {
+			console.log('Running output full name');
+			if (this.name == '') {
+				return '';
+			}
+			return `${this.name} Osrečki`;
+		},
+		computedTime() {
+			const now = new Date();
+			const hours = now.getHours();
+			const minutes = now.getMinutes();
+			const seconds =
+				now.getSeconds() < 10
+					? '0' + now.getSeconds()
+					: now.getSeconds();
+			const time = `${hours}:${minutes}:${seconds}`;
+			return time;
 		},
 	},
 });
 
-app.mount('#events');
+// `this` points to the vm instance
+const vm = app.mount('#events');
+// vm.counter
+// vm.$data.counter;
