a20a946245fd234db2b49eab4f770d5e7baed87a
Matija Osrecki <matija.osrecki@gmail.com>
2021-03-04 17:14:31 +0100

    :alembic:

---
+++ 19-composition-api/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<section class="container">
+		<h2>{{ userName }}</h2>
+	</section>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	components: {},
+	props: [],
+	// data() {
+	// 	return {
+	// 		userName: 'Maximilian',
+	// 	};
+	// },
+	setup() {
+		const uName = ref('Matija');
+		console.log(uName);
+
+		setTimeout(() => {
+			uName.value = 'Lovro';
+		}, 2000);
+
+		return { userName: uName };
+	},
+};
+</script>
+
+<style>
+* {
+	box-sizing: border-box;
+}
+
+html {
+	font-family: sans-serif;
+}
+
+body {
+	margin: 0;
+}
+
+.container {
+	margin: 3rem auto;
+	max-width: 30rem;
+	border-radius: 12px;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	padding: 1rem;
+	text-align: center;
+}
+</style>
