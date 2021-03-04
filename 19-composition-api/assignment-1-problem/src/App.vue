489da3dbbfe5038c4bfbfdaeba94287808c5a9e0
Matija Osrecki <matija.osrecki@gmail.com>
2021-03-04 20:50:16 +0100

    improved assignment :pencil2:

--- 19-composition-api/assignment-1-problem/src/App.vue
+++ 19-composition-api/assignment-1-problem/src/App.vue
@@ -2,9 +2,9 @@
 	<h2>My Course Goal</h2>
 	<!-- Task 1: Output your main course goal with help of the composition API -->
 	<!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-	<h3 v-if="goal.goalShown">{{ goal.courseGoal }}</h3>
+	<h3 v-if="courseData.goalShown">{{ courseData.courseGoal }}</h3>
 	<!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-	<button @click="goal.toggleGoalVisibility">Toggle Goal</button>
+	<button @click="toggleGoalVisibility">Toggle Goal</button>
 	<!-- Task 3: Manage data in three ways -->
 	<!-- => Separate refs -->
 	<!-- => Ref Object -->
@@ -14,56 +14,79 @@
 
 <script>
 import { reactive } from 'vue';
+// import { ref } from 'vue';
+
 export default {
+	//* Options API
+	// data() {
+	// 	return {
+	// 		goalShown: false,
+	// 		courseGoal: 'different strokes for different folkes',
+	// 	};
+	// },
+	// methods: {
+	// 	toggleGoalVisibility() {
+	// 		this.goalShown = !this.goalShown;
+	// 	},
+	// },
+	//* reactive
 	setup() {
-		const goal = reactive({
-			courseGoal: 'fuck hoes, get monney',
-			goalShown: true,
-			toggleGoalVisibility() {
-				this.goalShown = !this.goalShown;
-			},
+		const courseData = reactive({
+			courseGoal: 'different strokes for different folkes',
+			goalShown: false,
 		});
 
-		return {
-			goal,
-		};
+		function toggleGoalVisibility() {
+			courseData.goalShown = !courseData.goalShown;
+		}
+		return { courseData, toggleGoalVisibility };
 	},
+	//* reactive with toRefs
 	// setup() {
-	// 	const goal = reactive({
-	// 		courseGoal: 'fuck hoes, get monney',
-	// 		goalShown: true,
+	// 	const courseData = reactive({
+	// 		courseGoal: 'different strokes for different folkes',
+	// 		goalShown: false,
 	// 		toggleGoalVisibility() {
 	// 			this.goalShown = !this.goalShown;
 	// 		},
 	// 	});
-
-	// 	const goalRef = toRefs(goal);
-
+	// 	const goalRef = toRefs(courseData);
 	// 	return {
 	// 		courseGoal: goalRef.courseGoal,
 	// 		goalShown: goalRef.goalShown,
 	// 		toggleGoalVisibility: goalRef.toggleGoalVisibility,
 	// 	};
 	// },
+	//* ref object
 	// setup() {
-	// 	const goal = ref({
-	// 		courseGoal: 'fuck hoes, get monney',
-	// 		goalShown: true,
+	// 	const courseData = ref({
+	// 		courseGoal: 'different strokes for different folkes',
+	// 		goalShown: false,
 	// 		toggleGoalVisibility() {
 	// 			this.goalShown = !this.goalShown;
 	// 		},
 	// 	});
-
-	// 	return { goal };
+	// 	return { courseData };
 	// },
+	//* ref object 2
 	// setup() {
-	// 	const courseGoal = ref('fuck hoes');
-	// 	const goalVisibility = ref(true);
+	// 	const courseData = ref({
+	// 		courseGoal: 'different strokes for different folkes',
+	// 		goalShown: false,
+	// 	});
 
+	// 	function toggleGoalVisibility() {
+	// 		courseData.value.goalShown = !courseData.value.goalShown;
+	// 	}
+	// 	return { courseData, toggleGoalVisibility };
+	// },
+	//* sepereate refs
+	// setup() {
+	// 	const courseGoal = ref('different strokes for different folkes');
+	// 	const goalVisibility = ref(false);
 	// 	const toggleGoalVisibility = () => {
 	// 		goalVisibility.value = !goalVisibility.value;
 	// 	};
-
 	// 	return {
 	// 		courseGoal,
 	// 		toggleGoalVisibility,
